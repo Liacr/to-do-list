08683d3c819f13e7ffb1f7b31261abe10a4081aa
Yuri Lima <yurilima742@yahoo.com>
2024-07-28 16:48:15 -0300

    refactor: centralizando add button e colocando links em row no footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,18 +11,28 @@ const Footer: React.FC<FooterProps> = ({ linkedinSvg }) => {
       <h2 className={style.title}>Projeto desenvolvido por:</h2>
 
       <section className={style.sectionFooter}>
-        <article>
+        <article className={style.articleFooter}>
           <a href="https://www.linkedin.com/in/lianna-ribeiro-3366b9137/" className={style.linkedinLink} target='_blank'>
             <img src={linkedinSvg} alt="LinkedIn" className={style.linkedinIcon} />
           </a>
-          <h3 className={style.footerTexto}>Lianna Ribeiro</h3>
+
+          <a href="https://www.linkedin.com/in/lianna-ribeiro-3366b9137/" className={style.footerTexto} target='_blank'>
+            <h3>
+              Lianna Ribeiro
+            </h3>
+          </a>
         </article>
 
-        <article>
+        <article className={style.articleFooter}>
           <a href="https://www.linkedin.com/in/yuri-lima7/" className={style.linkedinLink} target='_blank'>
             <img src={linkedinSvg} alt="LinkedIn" className={style.linkedinIcon} />
           </a>
-          <h3 className={style.footerTexto}>Yuri Lima</h3>
+
+          <a href="https://www.linkedin.com/in/yuri-lima7/" className={style.footerTexto} target='_blank'>
+            <h3 className={style.footerTexto}>
+              Yuri Lima
+            </h3>
+          </a>
         </article>
       </section>
     </footer>
